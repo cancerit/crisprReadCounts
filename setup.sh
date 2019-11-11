172d4681e98c602a61d149124061d7792e6536db
Rebecca Shepherd <rpe@sanger.ac.uk>
2019-11-11 12:00:41 +0000

    Changed install code

--- setup.sh
+++ setup.sh
@@ -21,85 +21,59 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##########LICENCE##########
 
-get_file () {
-# output, source
-  if hash curl 2>/dev/null; then
-    curl --insecure -sS -o $1 -L $2
-  else
-    wget -nv -O $1 $2
-  fi
-}
+set -xe
 
-if [[ ($# -ne 1 && $# -ne 2) ]] ; then
-  echo "Please provide an installation path and optionally perl lib paths to allow, e.g."
-  echo "  ./setup.sh /opt/myBundle"
-  echo "OR all elements versioned:"
-  exit 0
+if [[ -z "${TMPDIR}" ]]; then
+  TMPDIR=/tmp
 fi
 
+set -u
+
+if [ "$#" -lt "1" ] ; then
+  echo "Please provide an installation path such as /opt/CASM"
+  exit 1
+fi
+
+
+# get path to this script
+SCRIPT_PATH=`dirname $0`;
+SCRIPT_PATH=`(cd $SCRIPT_PATH && pwd)`
+
+# get the location to install to
 INST_PATH=$1
-
-if [[ $# -eq 2 ]] ; then
-  CGP_PERLLIBS=$2
-fi
-
-CPU=`grep -c ^processor /proc/cpuinfo`
-if [[ $? -eq 0 ]]; then
-  if [[ $CPU -gt 6 ]]; then
-    CPU=6
-  fi
-else
-  CPU=1
-fi
-echo "Max compilation CPUs set to $CPU"
-
-INST_PATH=$1
+mkdir -p $1
+INST_PATH=`(cd $1 && pwd)`
+echo $INST_PATH
 
 # get current directory
 INIT_DIR=`pwd`
 
-set -e
-
-# cleanup inst_path
-mkdir -p $INST_PATH/bin
-cd $INST_PATH
-INST_PATH=`pwd`
-cd $INIT_DIR
-
-# make sure that build is self contained
-PERLROOT=$INST_PATH/lib/perl5
-
-# allows user to knowingly specify other PERL5LIB areas.
-if [ -z ${CGP_PERLLIBS+x} ]; then
-  export PERL5LIB="$PERLROOT"
-else
-  export PERL5LIB="$PERLROOT:$CGP_PERLLIBS"
-fi
-
-#add bin path for install tests
-export PATH=$INST_PATH/bin:$PATH
-
-#create a location to build dependencies
 SETUP_DIR=$INIT_DIR/install_tmp
-mkdir -p $SETUP_DIR
-
+mkdir -p $INST_PATH/bin $SETUP_DIR
 cd $SETUP_DIR
 
-echo -n "Installing cgpreadcounts ..."
-cd $INIT_DIR
-perl Makefile.PL INSTALL_BASE=$INST_PATH
-make
-make test
-make install
+# make sure tools installed can see the install loc of libraries
+set +u
+export LD_LIBRARY_PATH=`echo $INST_PATH/lib:$LD_LIBRARY_PATH | perl -pe 's/:\$//;'`
+export PATH=`echo $INST_PATH/bin:$BB_INST/bin:$PATH | perl -pe 's/:\$//;'`
+export MANPATH=`echo $INST_PATH/man:$BB_INST/man:$INST_PATH/share/man:$MANPATH | perl -pe 's/:\$//;'`
+export PERL5LIB=`echo $INST_PATH/lib/perl5:$PERL5LIB | perl -pe 's/:\$//;'`
+set -u
 
-# cleanup all junk
+# if cpanm is not installed
+if [ -z $(which cpanm) ]; then
+  echo "Can't find cpanm, trying to install.."
+  ## INSTALL CPANMINUS
+  set -eux
+  curl -sSL https://cpanmin.us/ > $SETUP_DIR/cpanm
+  perl $SETUP_DIR/cpanm --no-wget --no-interactive --notest --mirror http://cpan.metacpan.org -l $INST_PATH App::cpanminus
+  rm -f $SETUP_DIR/cpanm
+fi
+CPANM=`which cpanm`
+
+# Install crisprReadCounts
+$CPANM --no-wget --no-interactive --mirror http://cpan.metacpan.org --notest -l $INST_PATH --installdeps $SCRIPT_PATH/perl/
+$CPANM --no-wget --no-interactive --mirror http://cpan.metacpan.org -l $INST_PATH $SCRIPT_PATH/perl/
+
+cd $HOME
 rm -rf $SETUP_DIR
-
-
-echo
-echo
-echo "Please add the following to beginning of path:"
-echo "  $INST_PATH/bin"
-echo "Please add the following to beginning of PERL5LIB:"
-echo "  $PERLROOT"
-echo
