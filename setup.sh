dddc4e8d1067543c78eaf9d64c0e10a0b6df3e60
Rebecca Shepherd <rpe@sanger.ac.uk>
2019-11-11 11:54:58 +0000

    Changed install code

--- setup.sh
+++ setup.sh
@@ -85,21 +85,6 @@ mkdir -p $SETUP_DIR
 
 cd $SETUP_DIR
 
-## grab cpanm and stick in workspace, then do a self upgrade into bin:
-get_file $SETUP_DIR/cpanm https://cpanmin.us/
-perl $SETUP_DIR/cpanm -l $INST_PATH App::cpanminus
-CPANM=`which cpanm`
-
-cd $INIT_DIR
-
-echo -n "Installing Perl prerequisites ..."
-if ! ( perl -MExtUtils::MakeMaker -e 1 >/dev/null 2>&1); then
-    echo
-    echo "WARNING: Your Perl installation does not seem to include a complete set of core modules.  Attempting to cope with this, but if installation fails please make sure that at least ExtUtils::MakeMaker is installed.  For most users, the best way to do this is to use your system's package manager: apt, yum, fink, homebrew, or similar."
-fi
-
-$CPANM --mirror http://cpan.metacpan.org --notest -l $INST_PATH/ --installdeps . < /dev/null
-
 echo -n "Installing cgpreadcounts ..."
 cd $INIT_DIR
 perl Makefile.PL INSTALL_BASE=$INST_PATH
